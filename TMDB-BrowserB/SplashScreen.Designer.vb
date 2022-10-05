9f5a8335f16379db6a04c86d401e53d5d732de20
addingama <addingama@gmail.com>
2022-10-05 11:22:32 +0800

    Add project files.

---
+++ TMDB-BrowserB/SplashScreen.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SplashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pb_progress = New System.Windows.Forms.ProgressBar()
+        Me.lbl_loading = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Firebrick
+        Me.Label1.Location = New System.Drawing.Point(182, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(436, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "The Movie Database Browser"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.TMDB_BrowserB.My.Resources.Resources._92678_movie
+        Me.PictureBox1.Location = New System.Drawing.Point(276, 91)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(249, 195)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'pb_progress
+        '
+        Me.pb_progress.ForeColor = System.Drawing.Color.Firebrick
+        Me.pb_progress.Location = New System.Drawing.Point(68, 343)
+        Me.pb_progress.Name = "pb_progress"
+        Me.pb_progress.Size = New System.Drawing.Size(664, 23)
+        Me.pb_progress.Step = 1
+        Me.pb_progress.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.pb_progress.TabIndex = 2
+        '
+        'lbl_loading
+        '
+        Me.lbl_loading.AutoSize = True
+        Me.lbl_loading.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_loading.Location = New System.Drawing.Point(346, 308)
+        Me.lbl_loading.Name = "lbl_loading"
+        Me.lbl_loading.Size = New System.Drawing.Size(109, 20)
+        Me.lbl_loading.TabIndex = 3
+        Me.lbl_loading.Text = "Loading ... 0%"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'SplashScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 402)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.lbl_loading)
+        Me.Controls.Add(Me.pb_progress)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.Firebrick
+        Me.Name = "SplashScreen"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents pb_progress As ProgressBar
+    Friend WithEvents lbl_loading As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ TMDB-BrowserB/SplashScreen.vb
@@ -0,0 +1,28 @@
+﻿Public Class SplashScreen
+    ' Global variable untuk menampilkan nilai progress
+    Dim progress As Integer = 0
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub SplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    ' Fungsi yang akan dijalankan setiap interval timer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Menghitung progress terbaru
+        Dim newProgress = progress + 1
+        ' Mengupdate global variable progress supaya tidak stuk di angka 0
+        progress = newProgress
+
+        'Jika progress sama dengan 100 maka hentikan timer
+        If (progress = 100) Then
+            Timer1.Stop()
+        End If
+
+        ' set progress bar value menggunakan nilai newProgress
+        pb_progress.Value = newProgress
+        lbl_loading.Text = "Loading... " & newProgress & "%"
+    End Sub
+End Class
